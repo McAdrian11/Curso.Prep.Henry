7342f007e200930291ffe90e6a0df8eb195a8b6e
unknown <mcadriann11@gmail.com>
2022-02-25 01:59:12 -0500

    Homework 02 corregida

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Soy Adrian";
+const nuevaString = "Soy Adrian Morales";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 1;
